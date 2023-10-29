79071cacb59d6fb158ec4490719748f8f95063bc
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2023-10-29 18:00:28 +0530

    Print Ascii Values

---
+++ JavaExamples/JavaExamples/src/examples/PrintAsciiValueExample.java
@@ -0,0 +1,18 @@
+package examples;
+
+/**
+ * Created by Praveenkumar on 10/29/2023.
+ */
+public class PrintAsciiValueExample {
+
+    public static void main(String[] args) {
+// character whose ASCII value to be found
+        char ch1 = 'a';
+        char ch2 = 'b';
+// variable that stores the integer value of the character
+        int asciivalue1 = ch1;
+        int asciivalue2 = ch2;
+        System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
+        System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);
+    }
+}
